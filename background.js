bd16aeb16f5cbc2f3f221707dc236439347e0eb8
hakanyalniz <hakan_yalniz@live.com>
2026-01-30 02:33:07 +0300

    Huge amount of code was refactored and hours of debugging was done. Now the extension does not clash with other extensions that require download permission.

--- background.js
+++ background.js
@@ -1,24 +1,46 @@
-chrome.downloads.onDeterminingFilename.addListener(checkToggle);
-
-function checkToggle(downloadItem, suggest) {
-  chrome.storage.local.get("toggleSwitch").then((result) => {
-    const { toggleSwitch } = result;
-
-    if (!toggleSwitch) {
-      suggest();
-      return;
-    }
-
-    changeFileName(downloadItem, suggest);
-  });
+// Wrapper function for changeFileName, so we can safely add or remove listeners to it
+function onDetermine(downloadItem, suggest) {
+  changeFileName(downloadItem, suggest);
   return true;
 }
 
-async function getCurrentTab() {
+// When the browser is first opened, check if toggleSwitch is on or off, and add listener based on that
+chrome.runtime.onStartup.addListener(() => {
+  checkToggle();
+});
+
+// Catches the message from popup for the toggle change
+chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
+  if (message.type === "TOGGLE_CHANGED") {
+    checkToggle();
+    return true;
+  }
+});
+
+// After fetching toggleSwitch, add or remove listener based on its boolean value
+// Also make sure there is no listener or there is a listener before removing or adding them
+function checkToggle() {
+  chrome.storage.local.get("toggleSwitch").then(({ toggleSwitch }) => {
+    if (toggleSwitch) {
+      if (!chrome.downloads.onDeterminingFilename.hasListener(onDetermine)) {
+        chrome.downloads.onDeterminingFilename.removeListener(onDetermine);
+        chrome.downloads.onDeterminingFilename.addListener(onDetermine);
+      }
+    } else {
+      if (chrome.downloads.onDeterminingFilename.hasListener(onDetermine)) {
+        chrome.downloads.onDeterminingFilename.removeListener(onDetermine);
+      }
+    }
+  });
+}
+
+function getCurrentTab() {
   let queryOptions = { active: true, lastFocusedWindow: true };
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+  return chrome.tabs.query(queryOptions).then((result) => {
+    let [tab] = result;
+    return tab;
+  });
 }
 
 function changeFileName(downloadItem, suggest) {
@@ -59,6 +81,7 @@ function changeFileName(downloadItem, suggest) {
       filename: safeTitle + extension,
     });
   });
+  return true;
 }
 
 // Make title more customizable
