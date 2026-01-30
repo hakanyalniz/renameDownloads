e287d8998d05a90204960facccdfa01c4309da67
hakanyalniz <hakan_yalniz@live.com>
2026-01-30 03:47:29 +0300

    It appears there is a bug with chrome download listener, it is not working as expected. Rolling back to previous solution.

--- popup.js
+++ popup.js
@@ -20,9 +20,9 @@ function sendChromeMessage(switchButton, indicatorBox) {
     // Update local storage
     await chrome.storage.local.set({ toggleSwitch: !toggleSwitch });
 
-    chrome.runtime.sendMessage({
-      type: "TOGGLE_CHANGED",
-    });
+    // chrome.runtime.sendMessage({
+    //   type: "TOGGLE_CHANGED",
+    // });
 
     // it is inverted, because we just clicked the switch
     updateIndicator(indicatorBox, !toggleSwitch);
